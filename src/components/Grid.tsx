7e90b7df611775939223a3565f074558d72133d8
melchor <arantza.melchor@pwc.com>
2020-10-17 22:05:43 +0400

    Refactored grid items

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -8,6 +8,93 @@ interface Props {
   text: string;
 }
 
+const getMappedPosts = (posts: any, input: string) => {
+  let mappedPosts = [];
+  if (input === 'Users') {
+    mappedPosts = posts.map((item: any) => (
+      <div className="grid-item" key={item.id}>
+        <div className="grid-item-image">
+          <img src={item.avatar_url} alt="img" width="100" height="100" />
+        </div>
+        <div className="grid-item-text">
+          Name:
+          {' '}
+          {item.login}
+          {' '}
+          <br />
+          <a href={item.html_url} target="_blank" rel="noopener noreferrer">
+            Link to user account
+          </a>
+          {' '}
+        </div>
+      </div>
+    ));
+  } else if (input === 'Repositories') {
+    mappedPosts = posts.map((item: any) => (
+      <div className="grid-item" key={item.id}>
+        <div className="grid-item-text">
+          Name:
+          {' '}
+          {item.name}
+          {' '}
+          <br />
+          Language:
+          {' '}
+          {item.language}
+          {' '}
+          <br />
+          Stars:
+          {' '}
+          {item.stargazers_count}
+          {' '}
+          <br />
+          Forks:
+          {' '}
+          {item.forks_count}
+          {' '}
+          <br />
+          <a href={item.html_url} target="_blank" rel="noopener noreferrer">
+            Link to repository
+          </a>
+          {' '}
+        </div>
+      </div>
+    ));
+  } else {
+    mappedPosts = posts.map((item: any) => (
+      <div className="grid-item" key={item.id}>
+        <div className="grid-item-text">
+          Title:
+          {' '}
+          {item.title}
+          {' '}
+          <br />
+          State:
+          {' '}
+          {item.state}
+          {' '}
+          <br />
+          Comments:
+          {' '}
+          {item.comments}
+          {' '}
+          <br />
+          Creation Date:
+          {' '}
+          {item.created_at}
+          {' '}
+          <br />
+          <a href={item.html_url} target="_blank" rel="noopener noreferrer">
+            Link to issue
+          </a>
+          {' '}
+        </div>
+      </div>
+    ));
+  }
+  return mappedPosts;
+};
+
 const Grid = (props: Props) => {
   const {
     posts, status, input, text,
@@ -17,88 +104,7 @@ const Grid = (props: Props) => {
     let mappedPosts = [];
     let message;
     if (!_.isEmpty(posts)) {
-      if (input === 'Users') {
-        mappedPosts = posts.map((item: any) => (
-          <div className="grid-item" key={item.id}>
-            <div className="grid-item-image">
-              <img src={item.avatar_url} alt="img" width="100" height="100" />
-            </div>
-            <div className="grid-item-text">
-              Name:
-              {' '}
-              {item.login}
-              {' '}
-              <br />
-              <a href={item.html_url} target="_blank" rel="noopener noreferrer">
-                Link to user account
-              </a>
-              {' '}
-            </div>
-          </div>
-        ));
-      } else if (input === 'Repositories') {
-        mappedPosts = posts.map((item: any) => (
-          <div className="grid-item" key={item.id}>
-            <div className="grid-item-text">
-              Name:
-              {' '}
-              {item.name}
-              {' '}
-              <br />
-              Language:
-              {' '}
-              {item.language}
-              {' '}
-              <br />
-              Stars:
-              {' '}
-              {item.stargazers_count}
-              {' '}
-              <br />
-              Forks:
-              {' '}
-              {item.forks_count}
-              {' '}
-              <br />
-              <a href={item.html_url} target="_blank" rel="noopener noreferrer">
-                Link to repository
-              </a>
-              {' '}
-            </div>
-          </div>
-        ));
-      } else {
-        mappedPosts = posts.map((item: any) => (
-          <div className="grid-item" key={item.id}>
-            <div className="grid-item-text">
-              Title:
-              {' '}
-              {item.title}
-              {' '}
-              <br />
-              State:
-              {' '}
-              {item.state}
-              {' '}
-              <br />
-              Comments:
-              {' '}
-              {item.comments}
-              {' '}
-              <br />
-              Creation Date:
-              {' '}
-              {item.created_at}
-              {' '}
-              <br />
-              <a href={item.html_url} target="_blank" rel="noopener noreferrer">
-                Link to issue
-              </a>
-              {' '}
-            </div>
-          </div>
-        ));
-      }
+      mappedPosts = getMappedPosts(posts, input);
     } else {
       message = <p>{text}</p>;
     }
